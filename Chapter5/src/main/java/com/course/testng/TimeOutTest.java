040a18137df3055a62647f097f726b3ca12fd71d
fusanping <fspb124@163.com>
2024-10-21 17:45:35 +0800

    解决无法切换ä»分支代码

--- Chapter5/src/main/java/com/course/testng/TimeOutTest.java
+++ Chapter5/src/main/java/com/course/testng/TimeOutTest.java
@@ -1,8 +1,21 @@
 package com.course.testng;
-/** 
- * @author fusanping 
- * @date 2024年10月21日 12:41 
+
+import org.testng.annotations.Test;
+
+/**
+ * @author fusanping
+ * @date 2024年10月21日 12:41
  */
- 
+
 public class TimeOutTest {
+    @Test(timeOut = 3000)
+    public void testSuccess() throws InterruptedException {
+        Thread.sleep(2000);
+    }
+    @Test(timeOut = 2000)
+    public void testFailed() throws InterruptedException {
+
+
+        Thread.sleep(3000);
+    }
 }
